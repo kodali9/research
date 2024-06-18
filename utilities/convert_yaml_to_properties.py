8644a6bda4ac2b7867273794febcf12a8286503b
kodali9 <kodali9@gmail.com>
2024-06-18 12:37:34 -0400

    Update convert_yaml_to_properties.py

--- utilities/convert_yaml_to_properties.py
+++ utilities/convert_yaml_to_properties.py
@@ -1,24 +1,40 @@
+import os
 import yaml
-
-def flatten_dict(d, parent_key='', sep='.'):
-    items = []
-    for k, v in d.items():
-        new_key = f"{parent_key}{sep}{k}" if parent_key else k
-        if isinstance(v, dict):
-            items.extend(flatten_dict(v, new_key, sep=sep).items())
-        else:
-            items.append((new_key, v))
-    return dict(items)
+from pathlib import Path
 
 def yaml_to_properties(yaml_file, properties_file):
-    with open(yaml_file, 'r') as y_file:
-        yaml_content = yaml.safe_load(y_file)
+    with open(yaml_file, 'r') as yf:
+        yaml_content = yaml.safe_load(yf)
     
-    flat_dict = flatten_dict(yaml_content)
-    
-    with open(properties_file, 'w') as p_file:
-        for key, value in flat_dict.items():
-            p_file.write(f"{key}={value}\n")
+    properties_content = []
 
-# Example usage
-yaml_to_properties('application.yaml', 'application.properties')
+    def recurse_yaml(content, parent_key=''):
+        for key, value in content.items():
+            full_key = f"{parent_key}.{key}" if parent_key else key
+            if isinstance(value, dict):
+                recurse_yaml(value, full_key)
+            else:
+                properties_content.append(f"{full_key}={value}")
+
+    recurse_yaml(yaml_content)
+
+    with open(properties_file, 'w') as pf:
+        pf.write('\n'.join(properties_content))
+
+def convert_folder(folder_path):
+    folder = Path(folder_path)
+    yaml_files = folder.glob('**/application.yaml')
+
+    for yaml_file in yaml_files:
+        properties_file = yaml_file.with_suffix('.properties')
+        yaml_to_properties(yaml_file, properties_file)
+        print(f"Converted {yaml_file} to {properties_file}")
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Convert application.yaml files to application.properties')
+    parser.add_argument('folder', type=str, help='The folder containing application.yaml files')
+
+    args = parser.parse_args()
+    convert_folder(args.folder)
